9135dfd23422e28baea19cac7787c46d8ef9f5f7
Taremwa29 <rodneytaremwa29@gmail.com>
2022-07-01 22:47:47 +0300

    new dash

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 import sqlite3, datetime
 from flask import *
-import resources.modules.users as users
-
-# load models/verify.py
 from resources.modules.ylearnmodules import *
 
 app = Flask(__name__)
 app.secret_key = "edutechhasasecretS"
-
-con = sqlite3.connect('resources/databank/users/db.db3',
-                      check_same_thread=False)
-con.row_factory = dict_factory
-cur = con.cursor()
 
 
 @app.route('/')
@@ -39,19 +31,29 @@ def pay():
 
 @app.route('/verify')
 def verify():
-    return verify_trans(con, cur, request.args.get('transaction_id'))
+    return verify_trans(request.args.get('transaction_id'))
 
 
 @app.route("/signup", methods=["GET","POST"])
 def signup():
-    return signup_(cur=cur, request=request)
+    return signup_(request=request)
 
 #cur.execute(''' ''')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     account_type = request.args.get('as'); username = request.form.get("username"); passkey  =  request.form.get("password")
-    return users.login(cur=cur, account_type=account_type, username=username, passkey=passkey)
+    return login_(account_type=account_type, username=username, passkey=passkey)
+
+@app.route('/sendMessage')
+def sendMessage():
+    return sendMessage_(request.args.get('method'), request.args.get('sender'), request.args.get('receiver'), request.args.get('message'))
+
+@app.route('/studentlogin')
+def studentLogin():
+    return studentLogin_(request)
+
+
 
 # flask debug mode
 if __name__ == "__main__":
--- resources/modules/users.py
+++ resources/modules/users.py
@@ -1,5 +1,9 @@
+#%%
 from flask import *
-def login(cur=None, account_type=None, username=None, passkey=None):
+import resources.modules.database as db
+#%%
+
+def login_(account_type=None, username=None, passkey=None):
     #Return login forms
     if request.method == "GET":
         return render_template("/landing website/pages-login.html")
@@ -10,63 +14,92 @@ def login(cur=None, account_type=None, username=None, passkey=None):
         #Parent Account
          if account_type == "parent":
             '''SQL LOGIN WITH USERNAME AND PASSWORD THEN FETCH ROWS FROM DATABASE'''
-            cur.execute(f'SELECT * FROM parents WHERE Username="{username}" AND Pass="{passkey}";')
-            user = cur.fetchall()
+            user = db.runDBQuery(db.users_db, f'SELECT * FROM parents WHERE Username="{username}" AND Pass="{passkey}";')
             if len(user) == 1:
-                print(user)
-                session['username'] = user[0]['Username']
-                session['name'] = user[0]['Name']
-                return render_template("/parent's dashboard/dash.html")
+                # Store Student info in session cookies
+                session['name'] = user[0]['Name']; session['user'] = user[0]['Username']
+                return redirect(url_for('dashboard') + '?for=parent')
             else:
                 return "fail"
         #Student Account
          elif account_type == "student":
-            cur.execute(f'SELECT * FROM children WHERE Username="{username}" AND Pass="{passkey}";')
-            user = cur.fetchall()
+            user = db.runDBQuery(db.users_db, f'SELECT * FROM children WHERE Username="{username}" AND Pass="{passkey}";')
             if len(user) == 1:
-                session['user'] = user[0]['Username']
-                return "success"
+                # Store Student info in session cookies
+                session['name'] = user[0]['Name']; session['user'] = user[0]['Username']
+                return redirect(url_for('dashboard') + '?for=student')
             else:
                 return "fail"
-                
+
         #Account type not specified
          else:
             return "Account type not specified" 
 
-def signup_(cur=None, request=None):
+def signup_(request=None):
     #Return signup forms
     if request.method == "GET": 
         return render_template("pages-register.html")
 
     #Signup user
     elif request.method == "POST":
-
+        account_type = request.form['as']
         #Parent Account
-         if account_type == "parent":
+        if account_type == "parent":
             #SQL SIGNUP WITH USERNAME AND PASSWORD THEN STORE USER IN DATABASE'''
 
             # Get thew user submitted form data
-            username = request.form.get("email"); fullname = request.form.get("name"); passkey = request.form.get("password")
+            email = request.form.get("email"); fullname = request.form.get("name"); passkey = request.form.get("password")
             # Store data in database
-            cur.execute(f'INSERT INTO parents ("Username", "Name", "Email", "Children", "Pass") VALUES ("{username}","{fullname}", "fwack.rod", "[]", "{passkey}");')
-            print(con.commit())
+            db.runDBQuery(db.users_db, f'INSERT INTO parents ("Email", "Name", "Children", "Pass") VALUES ("{email}","{fullname}", "[]", "{passkey}");')
             return "Success"
 
         #Student Account
-         elif account_type == "student":
+        elif account_type == "student":
             #SQL SIGNUP WITH USERNAME AND PASSWORD THEN STORE USER IN DATABASE'''
 
-            # Get thew user submitted form data
-            #classid = request.form.get("class")
-            # #dob = request.form.get("DOB")#
-            # gender = request.form.get("gender")
-            # parentid = request.form.get("parentid") 
-
-            cur.execute(f'INSERT INTO children ("Username", "Name", "Class", "DOB", "Gender", "Parent") VALUES' f' ("{username}","{fullname}", "{classid}", "DOB", "{gender}", "{parentid}");')
-            print(con.commit())
-            return "Success"
+            # Get the form submitted data
+             classid = request.form.get("class");
+             username = request.form.get("username"); 
+             passkey = request.form.get("password"); 
+             gender=request.form.get('gender')
+             dob = request.form.get("dob")
+             parentid = session[username] 
+             
+             db.runDBQuery(db.users_db, f'INSERT INTO children ("Username", "Name", "Class", "DOB", "Gender", "Parent") VALUES' f' ("{username}","{fullname}", "{classid}", "DOB", "{gender}", "{parentid}");')
+             return "Success"
 
         #Account type not specified
-         else:
+        else:
             return "Account type not specified"
+
+# Student login from Parent's Dashboard
+def studentLogin_(request):
+    # Get children registered under parent account
+    childrenUnderParent = db.runDBQuery(db.users_db, f'''SELECT children FROM parents WHERE Username="{session['username']}";''')[0]['Children']
+    childrenUnderParent = json.loads(childrenUnderParent)
+     
+    # Find if child is registered under parent
+    if request.args.get('studentID') in childrenUnderParent:
+        # Store Student info in session cookies
+        session['user'] = request.args.get('studentId')
+        session['Name'] = db.runDBQuery(db.users_db, f'''SELECT Name FROM children WHERE Username="{session['user']}";''')[0]['Name']
+        return redirect(url_for('dashboard') + '?for=student')
+    else:
+        return "Error while logging in your child. Try agin in a while."
+
+
+def sendMessage_(method, sender, receiver, message):
+    if method == 'send':
+        # Store Message in database
+        sql_query = f'INSERT INTO chats (sender, receiver, message) VALUES ("{sender}", "{receiver}", "{message}");'
+        db.runDBQuery(db.users_db, sql_query)
+        return "Success"
+
+    elif method == 'get':
+        # Get Messages/Chat History from database
+        sql_query = f'SELECT * FROM chats WHERE (sender="{sender}" AND receiver="{receiver}") OR (sender="{receiver}" AND receiver="{sender}");'
+        return db.runDBQuery(db.users_db, sql_query)
+
+    
+
 
